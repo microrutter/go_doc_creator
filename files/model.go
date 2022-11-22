a8bef6170acefeaff4c0350bae08d0cbb610bf8d
microrutter <microrutter2514@hotmail.com>
2022-11-22 15:05:24 +0000

    Added in directory walkthrough to get all the files

--- files/model.go
+++ files/model.go
@@ -14,3 +14,17 @@ type Document struct {
 	Title    MainTitle
 	SubTitle []SubTitles
 }
+
+type File struct {
+	Name string
+	Doc  Document
+}
+
+type Directory struct {
+	Name  string
+	Files []File
+}
+
+type Directories struct {
+	ListDirect []Directory
+}
