d7cb81936e16a51e934e8343a48f2c72a3575572
microrutter <microrutter2514@hotmail.com>
2022-11-23 15:43:43 +0000

    refactor directories

--- files/directories.go
+++ files/directories.go
@@ -30,43 +30,12 @@ func (nd *Directories) WalkAllFilesInDir(dir string, log *log.Logger, confpath s
 			dirName := filepath.Base(filepath.Dir(path))
 			dd := NewDirectory()
 			dd.Name = dirName
-			// dd, appendDoc := checkIfDirectoryExists(*nd, dirName, log)
-			nf, appendFile := checkIfFileExists(dd.Files, info.Name(), log)
+			nf := NewFile()
+			nf.Name = info.Name()
 			nf.Doc = *doc
-			if appendFile {
-				dd.Files = append(dd.Files, *nf)
-			}
+			dd.Files = append(dd.Files, *nf)
 			nd.ListDirect = append(nd.ListDirect, *dd)
 		}
-
 		return nil
 	})
-}
-
-func checkIfDirectoryExists(d Directories, dn string, log *log.Logger) (*Directory, bool) {
-	for _, dir := range d.ListDirect {
-		if dir.Name == dn {
-			log.Printf("found %s", dn)
-			nd := &dir
-			return nd, false
-		}
-	}
-	log.Printf("Not found %s creating new directory", dn)
-	nd := NewDirectory()
-	nd.Name = dn
-	return nd, true
-}
-
-func checkIfFileExists(d []File, dn string, log *log.Logger) (*File, bool) {
-	for _, dir := range d {
-		if dir.Name == dn {
-			log.Printf("found %s", dn)
-			return &dir, false
-		}
-	}
-	log.Printf("Not found %s creating new File", dn)
-	nf := NewFile()
-	nf.Name = dn
-	log.Println(nf.Name)
-	return nf, true
 }
